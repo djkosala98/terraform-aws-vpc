2117b3a15ddc5170258ba6af3ee810dad3f5a89b
djkosala98 <77575000+djkosala98@users.noreply.github.com>
2024-02-25 20:49:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,13 +59,13 @@ resource "aws_default_network_acl" "default" {
   }
 }
 
-resource "aws_default_route_table" "route_table" {
-  count = var.enable_create_defaults ? 1 : 0
-
-  default_route_table_id = aws_vpc.vpc.default_route_table_id
-
-  tags = local.tags
-}
+#resource "aws_default_route_table" "route_table" {
+#  count = var.enable_create_defaults ? 1 : 0
+#
+#  default_route_table_id = aws_vpc.vpc.default_route_table_id
+#
+#  tags = local.tags
+#}
 
 resource "aws_default_security_group" "default" {
   count = var.enable_create_defaults ? 1 : 0
@@ -89,25 +89,25 @@ resource "aws_default_security_group" "default" {
   tags = local.tags
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
+#resource "aws_internet_gateway" "internet_gateway" {
+#  vpc_id = aws_vpc.vpc.id
+#
+#  tags = local.tags
+#}
 
-  tags = local.tags
-}
+#resource "aws_route_table" "public_routetable" {
+#  vpc_id = aws_vpc.vpc.id
+#
+#  tags = local.tags
+#}
 
-resource "aws_route_table" "public_routetable" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = local.tags
-}
-
-resource "aws_route" "public_route" {
-  depends_on = [aws_route_table.public_routetable]
-
-  route_table_id         = aws_route_table.public_routetable.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.internet_gateway.id
-}
+#resource "aws_route" "public_route" {
+#  depends_on = [aws_route_table.public_routetable]
+#
+#  route_table_id         = aws_route_table.public_routetable.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = aws_internet_gateway.internet_gateway.id
+#}
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
@@ -132,38 +132,38 @@ resource "aws_subnet" "public_subnet" {
   )
 }
 
-resource "aws_route_table_association" "public_routing_table" {
-  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
-  route_table_id = aws_route_table.public_routetable.id
-  count          = length(local.az)
-}
+#resource "aws_route_table_association" "public_routing_table" {
+#  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+#  route_table_id = aws_route_table.public_routetable.id
+#  count          = length(local.az)
+#}
 
-resource "aws_route_table" "private_routetable" {
-  count  = var.create_private_subnets ? 1 : 0
-  vpc_id = aws_vpc.vpc.id
+#resource "aws_route_table" "private_routetable" {
+#  count  = var.create_private_subnets ? 1 : 0
+#  vpc_id = aws_vpc.vpc.id
 
-  tags = local.tags
-}
+#  tags = local.tags
+#}
 
-resource "aws_route" "private_route" {
-  count      = var.create_private_subnets ? 1 : 0
-  depends_on = [aws_route_table.private_routetable]
+#resource "aws_route" "private_route" {
+#  count      = var.create_private_subnets ? 1 : 0
+#  depends_on = [aws_route_table.private_routetable]
+#
+#  route_table_id         = element(aws_route_table.private_routetable.*.id, 0)
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id         = element(aws_nat_gateway.nat.*.id, 0)
+#}
 
-  route_table_id         = element(aws_route_table.private_routetable.*.id, 0)
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.nat.*.id, 0)
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = cidrsubnet(
-    var.cidr_block,
-    8,
-    length(local.az) + count.index,
-  )
+#resource "aws_subnet" "private_subnet" {
+#  vpc_id = aws_vpc.vpc.id
+#  cidr_block = cidrsubnet(
+#    var.cidr_block,
+#    8,
+#    length(local.az) + count.index,
+#  )
 
   availability_zone       = element(local.az, count.index)
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
   count                   = var.create_private_subnets ? length(local.az) : 0
 
   tags = merge(
@@ -182,63 +182,63 @@ resource "aws_subnet" "private_subnet" {
   )
 }
 
-resource "aws_route_table_association" "private_routing_table" {
-  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id = element(aws_route_table.private_routetable.*.id, 0)
-  count          = var.create_private_subnets ? length(local.az) : 0
-}
+#resource "aws_route_table_association" "private_routing_table" {
+#  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+#  route_table_id = element(aws_route_table.private_routetable.*.id, 0)
+#  count          = var.create_private_subnets ? length(local.az) : 0
+#}
 
-data "aws_vpc_endpoint_service" "s3" {
-  count        = var.create_s3_vpc_endpoint ? 1 : 0
-  service      = "s3"
-  service_type = "Gateway"
-}
+#data "aws_vpc_endpoint_service" "s3" {
+#  count        = var.create_s3_vpc_endpoint ? 1 : 0
+#  service      = "s3"
+#  service_type = "Gateway"
+#}
 
-resource "aws_vpc_endpoint" "s3_vpc_endpoint" {
-  count        = var.create_s3_vpc_endpoint ? 1 : 0
-  vpc_id       = aws_vpc.vpc.id
-  service_name = element(data.aws_vpc_endpoint_service.s3.*.service_name, 0)
-  tags         = local.tags
-}
+#resource "aws_vpc_endpoint" "s3_vpc_endpoint" {
+#  count        = var.create_s3_vpc_endpoint ? 1 : 0
+#  vpc_id       = aws_vpc.vpc.id
+#  service_name = element(data.aws_vpc_endpoint_service.s3.*.service_name, 0)
+#  tags         = local.tags
+#}
 
-resource "aws_vpc_endpoint_route_table_association" "private_s3" {
-  count = var.create_s3_vpc_endpoint && var.create_private_subnets ? 1 : 0
+#resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+#  count = var.create_s3_vpc_endpoint && var.create_private_subnets ? 1 : 0
 
-  vpc_endpoint_id = element(aws_vpc_endpoint.s3_vpc_endpoint.*.id, 0)
-  route_table_id  = element(aws_route_table.private_routetable.*.id, count.index)
-}
+ # vpc_endpoint_id = element(aws_vpc_endpoint.s3_vpc_endpoint.*.id, 0)
+ # route_table_id  = element(aws_route_table.private_routetable.*.id, count.index)
+#}
 
-resource "aws_vpc_endpoint_route_table_association" "public_s3" {
-  count = var.create_s3_vpc_endpoint ? 1 : 0
+#resource "aws_vpc_endpoint_route_table_association" "public_s3" {
+#  count = var.create_s3_vpc_endpoint ? 1 : 0
 
-  vpc_endpoint_id = element(aws_vpc_endpoint.s3_vpc_endpoint.*.id, 0)
-  route_table_id  = element(aws_route_table.public_routetable.*.id, count.index)
-}
+#  vpc_endpoint_id = element(aws_vpc_endpoint.s3_vpc_endpoint.*.id, 0)
+#  route_table_id  = element(aws_route_table.public_routetable.*.id, count.index)
+#}
 
-resource "aws_eip" "nat" {
-  count = var.create_private_subnets ? 1 : 0
-  vpc   = true
+#resource "aws_eip" "nat" {
+#  count = var.create_private_subnets ? 1 : 0
+#  vpc   = true
 
-  tags = local.tags
-}
+# tags = local.tags
+#}
 
-resource "aws_nat_gateway" "nat" {
-  count         = var.create_private_subnets ? 1 : 0
-  allocation_id = element(aws_eip.nat.*.id, 0)
-  subnet_id     = aws_subnet.public_subnet[0].id
+#resource "aws_nat_gateway" "nat" {
+#  count         = var.create_private_subnets ? 1 : 0
+#  allocation_id = element(aws_eip.nat.*.id, 0)
+#  subnet_id     = aws_subnet.public_subnet[0].id
 
-  tags = local.tags
-}
+#  tags = local.tags
+#}
 
-resource "aws_route53_zone" "local" {
-  count   = var.create_private_hosted_zone ? 1 : 0
-  name    = "${var.environment}.local"
-  comment = "${var.environment} - route53 - local hosted zone"
+#resource "aws_route53_zone" "local" {
+#  count   = var.create_private_hosted_zone ? 1 : 0
+#  name    = "${var.environment}.local"
+#  comment = "${var.environment} - route53 - local hosted zone"
 
-  tags = local.tags
+#  tags = local.tags
 
-  vpc {
-    vpc_id = aws_vpc.vpc.id
-  }
-}
+#  vpc {
+#    vpc_id = aws_vpc.vpc.id
+#  }
+#}
 
